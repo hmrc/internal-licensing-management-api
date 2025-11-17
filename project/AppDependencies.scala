681e4ddf345085d6d0073fcce43ef0d2453c27d0
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-11-17 14:55:48 +0000

    APIS-7829 - Add extra OAS fields (#14)
    * APIS-7829 - Add extra OAS fields
    
    * APIS-7829 - PRBot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,22 +1,18 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
-  val commonDomainVersion = "0.19.0"
-
+  private val bootstrapVersion = "10.4.0"
+  val commonDomainVersion      = "0.19.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "api-platform-common-domain"  % commonDomainVersion,
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc" %% "api-platform-common-domain" % commonDomainVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"  % commonDomainVersion % Test
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"              % bootstrapVersion    % Test,
+    "uk.gov.hmrc" %% "api-platform-common-domain-fixtures" % commonDomainVersion % Test
   )
 
   val it = Seq.empty
